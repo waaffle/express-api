695fdb28dc71405f10cdb67061da033697cfe1d1
Yana Suslyakova <susljakovajana@yandex.ru>
2024-06-14 21:44:50 +0300

    Мидлвара для проверки токена

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const multer = require('multer');
 const { UserController } = require('../controllers');
+const authenticateToken = require('../middleware/auth');
 
 const uploadDestination = 'uploads';
 
@@ -16,9 +17,9 @@ const uploads = multer({storage: storage});
 
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
-router.get('/current', UserController.current)
-router.get('/users/:id', UserController.getUserById)
-router.put('/users/:id', UserController.updateUser)
+router.get('/current', authenticateToken, UserController.current)
+router.get('/users/:id', authenticateToken, UserController.getUserById)
+router.put('/users/:id', authenticateToken, UserController.updateUser)
 
 console.log(module);
 
