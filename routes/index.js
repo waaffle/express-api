086a19ff282c6ab2b6988a45d2e8d1d976c34d9f
Yana Suslyakova <susljakovajana@yandex.ru>
2024-06-14 20:16:48 +0300

    Контроллеры пользователя: регистрация и аутентификация

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
+const { UserController } = require('../controllers');
 
 const uploadDestination = 'uploads';
 
@@ -13,9 +14,11 @@ const storage = multer.diskStorage({
 
 const uploads = multer({storage: storage});
 
-router.get('/register', (request, response) => {
-    response.send('register')
-})
+router.post('/register', UserController.register)
+router.post('/login', UserController.login)
+router.get('/current', UserController.current)
+router.get('/users/:id', UserController.getUserById)
+router.put('/users/:id', UserController.updateUser)
 
 console.log(module);
 
