542046ccec2372d63bbf3c4116d7956e5dc7ce29
Yana Suslyakova <susljakovajana@yandex.ru>
2024-06-15 18:08:29 +0300

    Дописаны контроллеры пользователя. GetUserById, updateUser, current

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -69,13 +69,112 @@ const UserController = {
 
     },
     getUserById: async (req, res) => {
-        res.send('getUserById')
+        const { id } = req.params;
+        if (id.length!=24) {
+            return res.status(400).json({error: "Некорректный id"})
+        }
+
+        const userId = req.user.userId;
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    id: id
+                },
+                include: {
+                    followers: true,
+                    following: true
+                },
+            });
+
+            if (!user) {
+                return res.status(404).json({error: "Пользователь не найден"})
+            }
+
+            const isFollowing = await prisma.follows.findFirst({where: {
+                AND: [
+                    {followerId: userId},
+                    {followingId: id}
+                ]
+            }})
+
+            res.json({
+                ...user,
+                isFollowing: Boolean(isFollowing)
+            })
+
+        } catch (error) {
+            console.error("Error in getUserById", error);
+            return res.status(500).json({ error: "Internal server error" });
+        }
+
     },
     updateUser: async (req, res) => {
-        res.send('updateUser')
+        const { id } = req.params;
+
+        if (id.length!=24) {
+            return res.status(400).json({error: "Некорректный id"})
+        }
+
+        const { email, name, dataOfBirth, bio, location} = req.body;
+        let filePath;
+        if (req.file && req.file.path){
+            filePath = `/${req.file.path}`;
+        }
+
+        if (id !== req.user.userId) {
+            return res.status(403).json({error: "Доступ запрещен"})
+        }
+
+        try {
+            if (email) {
+                const existingUser = await prisma.user.findFirst({where: {email}});
+                if (existingUser && existingUser.id !== id) {
+                    return res.status(403).json({error: "Почта уже занята"});
+                }
+            }
+            const user = await prisma.user.update({where: {id},
+                data: {
+                    email: email || undefined,
+                    name: name || undefined,
+                    dateOfBirth: dataOfBirth || undefined,
+                    bio: bio || undefined, 
+                    location: location || undefined,
+                    avatarUrl: filePath || undefined
+                }});
+                res.json(user);
+
+        } catch (error) {
+            console.error("Errror in updateUser", error);
+            return res.status(500).json({error: "Internal server error"})  
+        }
+
     },
     current: async (req, res) => {
-        res.send('current')
+        try {
+            const user = await prisma.user.findUnique({where: {id: req.user.userId},
+            include: {
+                followers: {
+                    include: {
+                        follower: true
+                    }
+                },
+                following: {
+                    include: {
+                        following: true
+                    }
+                }
+            }});    
+            if (!user) {
+                return res.status(404).json({error: "Пользователь не найден"})
+            }
+            res.json(user)  
+
+        } catch (error) {
+            console.error("Errror in current", error);
+            return res.status(500).json({error: "Internal server error"})  
+        }
+        
     },
 }
 
