5bbed7c2fcc84f5c676934942e416d2a9763d106
Yana Suslyakova <susljakovajana@yandex.ru>
2024-07-04 21:38:33 +0300

    Включена обработка cors

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const fs = require('fs');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,18 +21,18 @@ app.use('/uploads', express.static('uploads'))
 
 app.use('/api', require('./routes'))
 
-if (!fs.existsSync('uploads')){
+if (!fs.existsSync('uploads')) {
   fs.mkdirSync('uploads');
 }
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
