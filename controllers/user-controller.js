a5c91d99802839b516e6b06068114dd7a35a70c2
Yana Suslyakova <susljakovajana@yandex.ru>
2024-08-16 23:42:49 +0300

    Исправила проблему с загрузкой файлов из-за неправильного пути

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,16 +8,16 @@ const jwt = require("jsonwebtoken")
 const UserController = {
     register: async (req, res) => {
         const { email, password, name } = req.body;
-        
-        if (!email || !password || !name){
-            return res.status(400).json({ error: "Все поля обязательные"})
+
+        if (!email || !password || !name) {
+            return res.status(400).json({ error: "Все поля обязательные" })
         }
 
         try {
-            const existingUser = await prisma.user.findUnique({ where: {email} });
+            const existingUser = await prisma.user.findUnique({ where: { email } });
 
             if (existingUser) {
-                return res.status(400).json({error: "Пользователь существует"})
+                return res.status(400).json({ error: "Пользователь существует" })
             }
 
             const hashedPassword = await bcrypt.hash(password, 10);
@@ -40,39 +40,39 @@ const UserController = {
         }
         catch (error) {
             console.error("Error in register", error);
-            res.status(500).json({error: "Internal server error"})
+            res.status(500).json({ error: "Internal server error" })
         }
     },
     login: async (req, res) => {
         const { email, password } = req.body;
-        if (!email || !password){
-            return res.status(400).json({ error: "Все поля обязательныe"})
+        if (!email || !password) {
+            return res.status(400).json({ error: "Все поля обязательныe" })
         }
-        try{
-            const user = await prisma.user.findUnique({where: {email}});
+        try {
+            const user = await prisma.user.findUnique({ where: { email } });
             if (!user) {
-                return res.status(400).json({ error: "Неверный логин или пароль"})
+                return res.status(400).json({ error: "Неверный логин или пароль" })
             }
             const valid = await bcrypt.compare(password, user.password);
 
-            if(!valid) {
-                return res.status(400).json({ error: "Неверный логин или пароль"})
+            if (!valid) {
+                return res.status(400).json({ error: "Неверный логин или пароль" })
             }
-            const token = jwt.sign(({userId: user.id}), process.env.SECRET_KEY);
+            const token = jwt.sign(({ userId: user.id }), process.env.SECRET_KEY);
 
-            res.json({token})
+            res.json({ token })
         }
-        catch(error){
+        catch (error) {
             console.error("Error in login", error);
             res.status(500).json("Internal server error")
         }
-        
+
 
     },
     getUserById: async (req, res) => {
         const { id } = req.params;
-        if (id.length!=24) {
-            return res.status(400).json({error: "Некорректный id"})
+        if (id.length != 24) {
+            return res.status(400).json({ error: "Некорректный id" })
         }
 
         const userId = req.user.userId;
@@ -89,15 +89,17 @@ const UserController = {
             });
 
             if (!user) {
-                return res.status(404).json({error: "Пользователь не найден"})
+                return res.status(404).json({ error: "Пользователь не найден" })
             }
 
-            const isFollowing = await prisma.follows.findFirst({where: {
-                AND: [
-                    {followerId: userId},
-                    {followingId: id}
-                ]
-            }})
+            const isFollowing = await prisma.follows.findFirst({
+                where: {
+                    AND: [
+                        { followerId: userId },
+                        { followingId: id }
+                    ]
+                }
+            })
 
             res.json({
                 ...user,
@@ -113,69 +115,73 @@ const UserController = {
     updateUser: async (req, res) => {
         const { id } = req.params;
 
-        if (id.length!=24) {
-            return res.status(400).json({error: "Некорректный id"})
+        if (id.length != 24) {
+            return res.status(400).json({ error: "Некорректный id" })
         }
 
-        const { email, name, dataOfBirth, bio, location} = req.body;
+        const { email, name, dataOfBirth, bio, location } = req.body;
         let filePath;
-        if (req.file && req.file.path){
-            filePath = `/${req.file.path}`;
+        if (req.file && req.file.path) {
+            filePath = req.file.path;
         }
 
         if (id !== req.user.userId) {
-            return res.status(403).json({error: "Доступ запрещен"})
+            return res.status(403).json({ error: "Доступ запрещен" })
         }
 
         try {
             if (email) {
-                const existingUser = await prisma.user.findFirst({where: {email}});
+                const existingUser = await prisma.user.findFirst({ where: { email } });
                 if (existingUser && existingUser.id !== id) {
-                    return res.status(403).json({error: "Почта уже занята"});
+                    return res.status(403).json({ error: "Почта уже занята" });
                 }
             }
-            const user = await prisma.user.update({where: {id},
+            const user = await prisma.user.update({
+                where: { id },
                 data: {
                     email: email || undefined,
                     name: name || undefined,
-                    dateOfBirth: dataOfBirth || undefined,
-                    bio: bio || undefined, 
+                    dataOfBirth: dataOfBirth || undefined,
+                    bio: bio || undefined,
                     location: location || undefined,
                     avatarUrl: filePath || undefined
-                }});
-                res.json(user);
+                }
+            });
+            res.json(user);
 
         } catch (error) {
             console.error("Errror in updateUser", error);
-            return res.status(500).json({error: "Internal server error"})  
+            return res.status(500).json({ error: "Internal server error" })
         }
 
     },
     current: async (req, res) => {
         try {
-            const user = await prisma.user.findUnique({where: {id: req.user.userId},
-            include: {
-                followers: {
-                    include: {
-                        follower: true
-                    }
-                },
-                following: {
-                    include: {
-                        following: true
+            const user = await prisma.user.findUnique({
+                where: { id: req.user.userId },
+                include: {
+                    followers: {
+                        include: {
+                            follower: true
+                        }
+                    },
+                    following: {
+                        include: {
+                            following: true
+                        }
                     }
                 }
-            }});    
+            });
             if (!user) {
-                return res.status(404).json({error: "Пользователь не найден"})
+                return res.status(404).json({ error: "Пользователь не найден" })
             }
-            res.json(user)  
+            res.json(user)
 
         } catch (error) {
             console.error("Errror in current", error);
-            return res.status(500).json({error: "Internal server error"})  
+            return res.status(500).json({ error: "Internal server error" })
         }
-        
+
     },
 }
 
